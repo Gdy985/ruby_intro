1b34a033d7bdeadefc940d6caebb7110aed7da84
Gdy985 <diyuangao2023@u.northwestern.edu>
2023-01-24 00:54:05 +0000

    second commit

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,8 +3,18 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+is_true = true
+is_false = false
+
+puts is_true
 
 # Boolean Expressions
+puts 3 > 2  # True
+puts 3 < 2  # False
+puts 3 == 2  # False
+puts 3 >= 2
+puts 3 <= 2
+puts 3 != 2  # Not equal to
 
 # If Conditional Logic
 
